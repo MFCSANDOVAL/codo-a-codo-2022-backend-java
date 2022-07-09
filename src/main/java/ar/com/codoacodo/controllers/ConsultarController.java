4c17cf7878c7394c70ecf698491cb1c27aa69cba
MFCSANDOVAL <mfc.sandoval@gmail.com>
2022-07-08 21:31:24 -0300

    actualizacion 7

--- src/main/java/ar/com/codoacodo/controllers/ConsultarController.java
+++ src/main/java/ar/com/codoacodo/controllers/ConsultarController.java
@@ -5,17 +5,19 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.sql.Date;
+
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
 import ar.com.codoacodo.connection.AdministradorDeConexiones;
 import ar.com.codoacodo.dto.Producto;
+
 
 
 
